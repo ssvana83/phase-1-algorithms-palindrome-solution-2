775e49badc6389a386c1352f2fbfc41dede69962
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2022-09-19 18:08:55 -0400

    commit tree cdf4b037b53b76e0cde57f8eb602f686a51f4b64

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  //iterate from the beginning to the middle
+  //racecar 7/2 == 3.5
+  //aaaaaa 6/3 ==3, if i iterate to index 3 it gets to middle of word
+  for (let i = 0; i < word.length / 2; i++) {
+    //check each letter to the cooresponding letter from the end
+    const j = word.length - 1 - i
+    const startChar = word[i]
+    const endChar = word[j]
+    //if any letter doesnt match, return false
+    if (startChar !== endChar) return false;
+  }
+  //or return true
+  return true;
 }
 
-/* 
+/*
+r a c e c a r
+0 1 2 3 4 5 6 
+i           j
+whenever we go through this loop I want i to move forward one and j
+to move backwards by one
+
   Add your pseudocode here
+  make a function that returns true if a word is a palindrome. Meaning if the first letter
+  is the same as the last letter and the second letter as same as the second letter
+  etc we will reach the middle and return true
+  abba = abba = true
+  pencil = licnep = false
 */
 
 /*
